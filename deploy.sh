9b9cef21a678bbfd23a148eb944e14e52580c3ff
周洋 <zhouyang02@qutoutiao.net>
2019-09-24 16:56:11 +0800

    feat: 修改ci

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:youngchou1997/ex1.git master:master
+git push -f git@github.com:youngchou1997/icework1.git master:gh-pages
 
 cd -
